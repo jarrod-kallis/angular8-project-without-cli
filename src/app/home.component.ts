4a17d4bc01492c03783c76e5414f9063d94f20e9
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-01-02 20:08:51 +0200

    Add life to the files

---
+++ src/app/home.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: '<h1>Home</h1>'
+})
+export class HomeComponent { }
---
+++ src/app/main.ts
@@ -0,0 +1,7 @@
+// Must be loaded first
+import '../polyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
